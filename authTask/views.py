e186ee1217441ccc00a4d9f6f0c8c7ad4ea21999
kalet chavez <kalet.svdev@gmail.com>
2024-02-02 22:28:14 -0600

    comienzo de intento de configuracion de tailwindcss y django

--- authTask/views.py
+++ authTask/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -7,8 +9,18 @@ def home( request: HttpRequest ):
     return render( request=request, template_name='home.html' )
 
 def login( request: HttpRequest ):
-    return render( request=request, template_name='login.html' )
+    if request.method == 'GET':
+        return render( request=request, template_name='login.html' )
+
 
 
 def register( request: HttpRequest ):
-    return render( request=request, template_name='register.html' )
+    if request.method == 'GET':
+        return render( request=request, template_name='register.html' )
+    elif request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User(
+                username=request.POST['username'],
+                password=request.POST['password1'])
+            user.save()
+            return redirect('')
