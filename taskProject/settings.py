55dc257175a83dd6fa7b2f9b536d9b43959a9347
kalet chavez <kalet.svdev@gmail.com>
2024-02-16 16:26:15 -0600

    creacion de configuraciones

--- taskProject/settings.py
+++ taskProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0qz7%$829-eyqu!i9pa8d(d(*0j3&-3nfk5hitlgq+4cd@_0*&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.24']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 STATIC_URL = BASE_DIR  / 'static'
 # Application definition
